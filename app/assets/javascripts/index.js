6c0916cfe0395f0a9d3cbfae8983fe65739971cb
James Ward <james@jamesward.com>
2020-02-21 13:06:40 -0700

    reconnect on error

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -43,100 +43,106 @@ document.addEventListener('DOMContentLoaded', () => {
     const arena = document.getElementById('arena');
     const scoreboard = document.getElementById('scoreboard');
 
-    const stringSource = new EventSource(maybeUpdatesUrl);
-    stringSource.onopen = () => { };
-    stringSource.onmessage = (message) => {
-      if (body.dataset.paused !== 'true') {
-        const data = JSON.parse(message.data);
-        //console.log(data);
+    function eventSource() {
+      const stringSource = new EventSource(maybeUpdatesUrl);
+      stringSource.onopen = () => { };
+      stringSource.onmessage = (message) => {
+        if (body.dataset.paused !== 'true') {
+          const data = JSON.parse(message.data);
+          //console.log(data);
 
-        title.textContent = data.name;
-
-        // arena setup
-        if (parseInt(arena.dataset.width) !== data.width || parseInt(arena.dataset.height) !== data.height) {
-          // dims changed
-          arena.dataset.width = data.width;
-          arena.dataset.height = data.height;
+          title.textContent = data.name;
 
           // arena setup
-          clear(arena);
+          if (parseInt(arena.dataset.width) !== data.width || parseInt(arena.dataset.height) !== data.height) {
+            // dims changed
+            arena.dataset.width = data.width;
+            arena.dataset.height = data.height;
 
-          arena.style.gridTemplateColumns = 'repeat(' + data.width + ', 1fr)';
+            // arena setup
+            clear(arena);
 
-          for (let y = 0; y < data.height; y++) {
-            for (let x = 0; x < data.width; x++) {
-              const cell = document.createElement('div');
-              cell.id = x + '-' + y;
-              cell.className = 'cell';
-              arena.appendChild(cell);
+            arena.style.gridTemplateColumns = 'repeat(' + data.width + ', 1fr)';
+
+            for (let y = 0; y < data.height; y++) {
+              for (let x = 0; x < data.width; x++) {
+                const cell = document.createElement('div');
+                cell.id = x + '-' + y;
+                cell.className = 'cell';
+                arena.appendChild(cell);
+              }
             }
           }
-        }
 
 
-        clear(arena, 'cell');
-        Array.prototype.slice.call(arena.getElementsByClassName('cell')).forEach(cell => cell.className = 'cell');
+          clear(arena, 'cell');
+          Array.prototype.slice.call(arena.getElementsByClassName('cell')).forEach(cell => cell.className = 'cell');
 
-        clear(scoreboard, 'score', true);
+          clear(scoreboard, 'score', true);
 
 
-        const sortPlayersByScore = (player1, player2) => player2[1].score - player1[1].score;
+          const sortPlayersByScore = (player1, player2) => player2[1].score - player1[1].score;
 
-        for (const [playerPath, player] of Object.entries(data.players).sort(sortPlayersByScore)) {
-          //println(player);
+          for (const [playerPath, player] of Object.entries(data.players).sort(sortPlayersByScore)) {
+            //println(player);
 
-          // profile image on grid
-          const cell = document.getElementById(player.x + '-' + player.y);
-          const img = document.createElement('img');
-          img.src = player.pic;
-          switch (player.direction) {
-            case 'N':
-              img.className = 'pic direction-n';
-              break;
-            case 'W':
-              img.className = 'pic direction-w';
-              break;
-            case 'S':
-              img.className = 'pic direction-s';
-              break;
-            case 'E':
-              img.className = 'pic direction-e';
-              break;
+            // profile image on grid
+            const cell = document.getElementById(player.x + '-' + player.y);
+            const img = document.createElement('img');
+            img.src = player.pic;
+            switch (player.direction) {
+              case 'N':
+                img.className = 'pic direction-n';
+                break;
+              case 'W':
+                img.className = 'pic direction-w';
+                break;
+              case 'S':
+                img.className = 'pic direction-s';
+                break;
+              case 'E':
+                img.className = 'pic direction-e';
+                break;
+            }
+            cell.appendChild(img);
+
+            if (player.wasHit) {
+              cell.className = cell.className + " hit";
+            }
+
+            // scores
+            const scoreLine = document.createElement('div');
+            scoreLine.className = 'score';
+
+            const scoreImg = document.createElement('img');
+            scoreImg.src = player.pic;
+            scoreLine.appendChild(scoreImg);
+
+            const name = document.createElement('span');
+            name.innerText = player.name;
+            name.className = 'name';
+            scoreLine.appendChild(name);
+
+            const score = document.createElement('span');
+            score.innerText = player.score;
+            score.className = 'num';
+            scoreLine.appendChild(score);
+
+            scoreboard.appendChild(scoreLine);
           }
-          cell.appendChild(img);
 
-          if (player.wasHit) {
-            cell.className = cell.className + " hit";
-          }
-
-          // scores
-          const scoreLine = document.createElement('div');
-          scoreLine.className = 'score';
-
-          const scoreImg = document.createElement('img');
-          scoreImg.src = player.pic;
-          scoreLine.appendChild(scoreImg);
-
-          const name = document.createElement('span');
-          name.innerText = player.name;
-          name.className = 'name';
-          scoreLine.appendChild(name);
-
-          const score = document.createElement('span');
-          score.innerText = player.score;
-          score.className = 'num';
-          scoreLine.appendChild(score);
-
-          scoreboard.appendChild(scoreLine);
         }
+      };
 
-      }
-    };
+      stringSource.onerror = (event) => {
+        console.error(event);
+        // try to reconnect
+        window.setTimeout(eventSource, 5000);
+      };
+    }
 
-    stringSource.onerror = (error) => {
-      // todo: reconnect
-      console.log(error);
-    };
+    eventSource();
+
   }
   else {
     // todo
