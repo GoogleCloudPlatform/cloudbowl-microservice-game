58ea14ae5a10f3f9283e2f8ce56db9dc133c092e
Ahmet Alp Balkan <ahmetb@google.com>
2020-03-20 12:06:05 -0700

    Add go sample
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

---
+++ samples/go/go.mod
@@ -0,0 +1,3 @@
+module github.com/GoogleCloudPlatform/cloudbowl-microservice-game/samples/go
+
+go 1.14
---
+++ samples/go/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	rand2 "math/rand"
+	"net/http"
+	"os"
+)
+
+func main() {
+	port := "8080"
+	if v := os.Getenv("PORT"); v != "" {
+		port = v
+	}
+	http.HandleFunc("/", handler)
+
+	log.Printf("starting server on port :%s", port)
+	err := http.ListenAndServe(":"+port, nil)
+	log.Fatalf("http listen error: %v", err)
+}
+
+func handler(w http.ResponseWriter, req *http.Request) {
+	if req.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		fmt.Fprint(w, "only POST method supported")
+		return
+	}
+
+	var v ArenaUpdate
+	defer req.Body.Close()
+	d := json.NewDecoder(req.Body)
+	d.DisallowUnknownFields()
+	if err := d.Decode(&v); err != nil {
+		log.Printf("WARN: failed to decode ArenaUpdate in response body: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	resp := play(v)
+	fmt.Fprint(w, resp)
+}
+
+func play(input ArenaUpdate) (response string) {
+	log.Printf("IN: %#v", input)
+
+	commands := []string{"F", "R", "L", "T"}
+	rand := rand2.Intn(4)
+	return commands[rand]
+}
