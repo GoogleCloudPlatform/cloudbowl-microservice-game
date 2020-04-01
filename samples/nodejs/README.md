d57276f12f2283a374374d36f464faafeb4ddc6e
James Ward <james@jamesward.com>
2020-04-01 10:21:54 -0600

    add nodejs sample

---
+++ samples/nodejs/README.md
@@ -0,0 +1,14 @@
+Cloud Bowl Sample - Scala Node.js
+---------------------------------
+
+To make changes, edit the `web.js` file.
+
+Run Locally (Dev):
+```
+npm install
+npm run start
+```
+
+Deploy:
+
+[![Run on Google Cloud](https://deploy.cloud.run/button.svg)](https://deploy.cloud.run)
---
+++ samples/nodejs/web.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.post('/', function (req, res) {
+  console.log(req.body);
+  const moves = ['F', 'T', 'L', 'R'];
+  res.send(moves[Math.floor(Math.random() * moves.length)]);
+});
+
+app.listen(process.env.PORT || 3000);
