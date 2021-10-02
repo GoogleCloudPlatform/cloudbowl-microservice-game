12789a1485fae493e0fe2cc1c566ade1c7a2ed5b
Dayal Nigam <57106200+dayalnigam@users.noreply.github.com>
2021-10-02 15:08:43 +0530

    Update main.py

--- samples/python/main.py
+++ samples/python/main.py
@@ -22,15 +22,14 @@ logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
-moves = ['F', 'T', 'L', 'R']
+moves = ['T','T','T','R']
 
 @app.route("/", methods=['POST'])
 def move():
     request.get_data()
     logger.info(request.json)
     while True:
-        for i in moves:
-            return i
+        return moves[random.randrange(len(moves))]
 
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
