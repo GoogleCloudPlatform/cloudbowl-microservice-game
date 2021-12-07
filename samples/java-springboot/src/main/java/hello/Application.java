4b3bd834ea7b0cefebd1521764429d5c7d65f796
James Ward <james@jamesward.com>
2021-12-07 16:14:12 -0700

    to bq

--- samples/java-springboot/src/main/java/hello/Application.java
+++ samples/java-springboot/src/main/java/hello/Application.java
@@ -1,10 +1,18 @@
 package hello;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.ServiceOptions;
+import com.google.cloud.bigquery.storage.v1.*;
+import com.google.protobuf.Descriptors;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+import java.time.Instant;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -12,6 +20,16 @@ import java.util.Random;
 @SpringBootApplication
 @RestController
 public class Application {
+
+  final String projectId = ServiceOptions.getDefaultProjectId();
+  final String datasetName = "snowball";
+  final String tableName = "events";
+
+  final WriteCommittedStream writeCommittedStream;
+
+  public Application() throws Descriptors.DescriptorValidationException, IOException, InterruptedException {
+    writeCommittedStream = new WriteCommittedStream(projectId, datasetName, tableName);
+  }
 
   static class Self {
     public String href;
@@ -56,9 +74,55 @@ public class Application {
   @PostMapping("/**")
   public String index(@RequestBody ArenaUpdate arenaUpdate) {
     System.out.println(arenaUpdate);
+
+    writeCommittedStream.send(arenaUpdate.arena); // fire & forget
+
     String[] commands = new String[]{"F", "R", "L", "T"};
     int i = new Random().nextInt(4);
     return commands[i];
+  }
+
+  static class WriteCommittedStream {
+
+    final JsonStreamWriter jsonStreamWriter;
+
+    public WriteCommittedStream(String projectId, String datasetName, String tableName) throws IOException, Descriptors.DescriptorValidationException, InterruptedException {
+
+      try (BigQueryWriteClient client = BigQueryWriteClient.create()) {
+
+        WriteStream stream = WriteStream.newBuilder().setType(WriteStream.Type.COMMITTED).build();
+        TableName parentTable = TableName.of(projectId, datasetName, tableName);
+        CreateWriteStreamRequest createWriteStreamRequest =
+                CreateWriteStreamRequest.newBuilder()
+                        .setParent(parentTable.toString())
+                        .setWriteStream(stream)
+                        .build();
+
+        WriteStream writeStream = client.createWriteStream(createWriteStreamRequest);
+
+        jsonStreamWriter = JsonStreamWriter.newBuilder(writeStream.getName(), writeStream.getTableSchema()).build();
+      }
+    }
+
+    public ApiFuture<AppendRowsResponse> send(Arena arena) {
+      Instant now = Instant.now();
+      JSONArray jsonArray = new JSONArray();
+
+      arena.state.forEach((url, playerState) -> {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("x", playerState.x);
+        jsonObject.put("y", playerState.y);
+        jsonObject.put("direction", playerState.direction);
+        jsonObject.put("wasHit", playerState.wasHit);
+        jsonObject.put("score", playerState.score);
+        jsonObject.put("player", url);
+        jsonObject.put("timestamp", now.getEpochSecond() * 1000 * 1000);
+        jsonArray.put(jsonObject);
+      });
+
+      return jsonStreamWriter.append(jsonArray);
+    }
+
   }
 
 }
